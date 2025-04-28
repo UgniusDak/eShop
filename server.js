ad1660f1f22e0cbb214a52729470186dcdd625ac
Ugnius Daktaras <ugniusdaktaras@gmail.com>
2025-04-28 18:14:36 +0300

    test

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
